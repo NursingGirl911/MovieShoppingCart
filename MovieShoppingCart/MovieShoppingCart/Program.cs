ed2090fb6183b049a6d90712e2632d28c8e40fb8
Brittany Martinez <>
2021-02-10 19:52:36 -0700

    This is module 5

--- MovieShoppingCart/MovieShoppingCart/Program.cs
+++ MovieShoppingCart/MovieShoppingCart/Program.cs
@@ -8,13 +8,14 @@ namespace MovieShoppingCart
     {
         static void Main(string[] args)
         {
-            DateTime myDateTimeVariable = new DateTime(1995, 04, 09, 11, 41, 00);
-            DateTime myDateTimeVariable2 = new DateTime(2019, 09, 25, 01, 11, 00);
+            int result = 0;
 
-            if (myDateTimeVariable < myDateTimeVariable2)
+            for(int x=1;x<21;x++) //3
             {
-                Console.WriteLine("Yes, the value is less");
+                result = result + x; //6
             }
+
+            Console.WriteLine(result); 
         }
     }
 }
