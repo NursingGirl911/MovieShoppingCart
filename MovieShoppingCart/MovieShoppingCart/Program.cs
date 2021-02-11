9bed0a4996dcd838577334757739dc32c6bcb514
Brittany Martinez <>
2021-02-10 22:02:00 -0700

    This is Module 4 Project corrected

--- MovieShoppingCart/MovieShoppingCart/Program.cs
+++ MovieShoppingCart/MovieShoppingCart/Program.cs
@@ -9,60 +9,22 @@ namespace MovieShoppingCart
     {
         static void Main(string[] args)
         {
-            string response = "";
-            List<User> userList = new List<User>();
-            List<Comment> commentList = new List<Comment>();
+            DateTime myFirstDateVariable = new DateTime(1995, 04, 09);
+            DateTime mySecondDateVariable = new DateTime(2019, 09, 25);
 
+            int result = DateTime.Compare(myFirstDateVariable, mySecondDateVariable);
 
-            while (response!="5")
+            //1 = First date is greater than the second
+            //0 = Means the two dates are equal
+            //-1 = First date is not greater than the second
+
+            if(result==1)
             {
-                Console.WriteLine("1. Create a User");
-                Console.WriteLine("2. Creat a Comment");
-                Console.WriteLine("3. Print the List of Users");
-                Console.WriteLine("4. Print the List of Comments");
-                Console.WriteLine("5. Exit");
-
-                Console.WriteLine("");
-                Console.WriteLine("Please select an option")
-                response = Console.ReadLine();
-
-                switch(response)
-                {
-                    case "1":
-                        User user = new User();
-                        user.Email = "myemail@somewhere.com";
-                        user.Password = "password";
-
-                        userList.Add(user);
-
-                        Console.WriteLine("You have created a user");
-                        break;
-                    case "2":
-                        Comment myComment = new Comment();
-                        myComment.CommentType = "Happy";
-
-                        commentList.Add(myComment);
-
-                        Console.WriteLine("You have created a comment");
-                        break;
-                    case "3":
-                        Console.WriteLine("This is the list of the users created:");
-
-                        foreach (var insideUser in userList)
-                        {
-                            Console.WriteLine("The user email is:" + insideUser.Email);
-                            Console.WriteLine("The user password is:" + insideUser.Password);
-                        }
-                        break;
-                    case "4":
-                        Console.WriteLine("This is the list of the comments created:");
-
-                        foreach (var insideComment in commentList)
-                        {
-                            Console.WriteLine("The comment type is:" + insideComment.CommentType);
-                        }
-                        break;
-                }
+                Console.WriteLine("The second date is greater");
+            }
+            else
+            {
+                Console.WriteLine("The first date is greater"); 
             }
         }
     }
