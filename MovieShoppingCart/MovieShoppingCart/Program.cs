6ecc8f84be66e80107abd5a5b97ba358b97000ee
Brittany Martinez <>
2021-02-10 20:12:14 -0700

    This is module 6 project

--- MovieShoppingCart/MovieShoppingCart/Program.cs
+++ MovieShoppingCart/MovieShoppingCart/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using MovieShoppingCart.DTOS;
 using MovieShoppingCart.MyClasses;
 
@@ -8,14 +9,41 @@ namespace MovieShoppingCart
     {
         static void Main(string[] args)
         {
-            int result = 0;
+            int[] myArray = new int[9];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
 
-            for(int x=1;x<21;x++) //3
+            Console.WriteLine("The values on the array are the following:");
+            foreach(var item in myArray)
             {
-                result = result + x; //6
+                Console.WriteLine(item + ", ");
             }
 
-            Console.WriteLine(result); 
+            List<string> myList = new List<string>();
+            myList.Add("Value 1");
+            myList.Add("Value 2");
+            myList.Add("Value 3");
+            myList.Add("Value 4");
+            myList.Add("Value 5");
+            myList.Add("Value 6");
+            myList.Add("Value 7");
+            myList.Add("Value 8");
+            myList.Add("Value 9");
+            myList.Add("Value 10");
+
+            Console.WriteLine("");
+            Console.WriteLine("The values on the list are the following:");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item + ", "); 
+            }
         }
     }
 }
